cd1b91532dfc734b2e48b41190ca4b8855a20953
rommel santiago <cocosanti@gmail.com>
2023-03-07 15:51:25 -0600

    Fix passing vars to provider

--- variables.tf
+++ variables.tf
@@ -3,3 +3,13 @@ variable "region" {
   description = "The was region"
   default     = "us-east-1"
 }
+
+variable "my_access_key" {
+  type = string
+  description = "The aws access key"
+}
+
+variable "my_secret_key" {
+  type = string
+  description = "The aws secret key"
+}
